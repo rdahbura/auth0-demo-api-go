c54afa7c89c6214ac06c30b48fd4a1fe2a61deda
Rudy Dahbura <rdahbura@gmail.com>
2021-11-21 14:56:51 -0800

    Code cleanup.

--- config/default.go
+++ config/default.go
@@ -3,28 +3,8 @@ package config
 import (
 	"fmt"
 	"os"
-	"time"
 
 	"github.com/joho/godotenv"
-)
-
-const (
-	ContextBearerToken = "BearerToken"
-)
-
-const (
-	DefaultClientTimeout = time.Second * 5
-	DefaultCtxTimeout    = time.Second * 5
-	DefaultIdleTimeout   = time.Second * 60
-	DefaultReadTimeout   = time.Second * 15
-	DefaultTokenLeeway   = time.Second * 30
-	DefaultWriteTimeout  = time.Second * 15
-)
-
-const (
-	MimeApplicationJson               = "application/json"
-	MimeApplicationXWwwFormUrlencoded = "application/x-www-form-urlencoded"
-	MimeTextHtml                      = "text/html"
 )
 
 var (
