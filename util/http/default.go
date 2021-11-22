d21c3387cb32076e119a6224afcc09933ddf9c8b
Rudy Dahbura <rdahbura@gmail.com>
2021-11-22 14:06:14 -0800

    Refactoring.

--- util/http/default.go
+++ util/http/default.go
@@ -1,139 +1,25 @@
 package http
 
 import (
-	"encoding/base64"
-	"encoding/json"
-	"errors"
-	"fmt"
-	"io"
 	"net/http"
-	"strings"
+	"sync"
 
 	"dahbura.me/api/config"
-
-	"github.com/gin-gonic/gin"
 )
 
-func DoRequest(req *http.Request) ([]byte, error) {
-	res, err := httpClient.Do(req)
-	if err != nil {
-		return nil, err
-	}
+var (
+	httpClient     *http.Client
+	httpClientOnce sync.Once
+)
 
-	defer res.Body.Close()
+func GetHttpClient() *http.Client {
+	httpClientOnce.Do(initHttpClient)
 
-	body, err := io.ReadAll(res.Body)
-	if err != nil {
-		return nil, err
-	}
-
-	return body, nil
+	return httpClient
 }
 
-func HandleError(c *gin.Context, err error) bool {
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"msg": err.Error()})
-		return true
+func initHttpClient() {
+	httpClient = &http.Client{
+		Timeout: config.DefaultClientTimeout,
 	}
-
-	return false
-}
-
-func HandleErrorMiddleware(c *gin.Context, err error) bool {
-	if err != nil {
-		c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"msg": err.Error()})
-		c.Error(err)
-		return true
-	}
-
-	return false
-}
-
-func SetAuthHeader(req *http.Request, at string) {
-	val := fmt.Sprintf("Bearer %s", at)
-	req.Header.Set("Authorization", val)
-}
-
-func TokenFromContext(c *gin.Context) (string, error) {
-	tokenValue, exists := c.Get(config.ContextBearerToken)
-	if !exists {
-		return "", fmt.Errorf("unable to find token")
-	}
-
-	token, ok := tokenValue.(string)
-	if !ok {
-		return "", errors.New("unable to convert token to string")
-	}
-
-	return token, nil
-}
-
-func TokenFromHeader(c *gin.Context) (string, error) {
-	header := c.GetHeader("Authorization")
-	if len(header) == 0 {
-		return "", errors.New("authorization header not found")
-	}
-
-	headerSegments := strings.Split(header, " ")
-	if len(headerSegments) != 2 {
-		return "", errors.New("authorization header segment count is incorrect")
-	}
-
-	schemeSegment := headerSegments[0]
-	if !strings.EqualFold(schemeSegment, "Bearer") {
-		return "", errors.New("authorization scheme not found")
-	}
-
-	tokenSegment := headerSegments[1]
-
-	return tokenSegment, nil
-}
-
-func VerifyScope(token string, scopesClaim string, scope string) error {
-	encPayload := strings.Split(token, ".")[1]
-	decPayload, err := base64.RawURLEncoding.DecodeString(encPayload)
-	if err != nil {
-		return err
-	}
-
-	payload := map[string]interface{}{}
-	err = json.Unmarshal(decPayload, &payload)
-	if err != nil {
-		return err
-	}
-
-	err = hasScope(payload, scopesClaim, scope)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func hasScope(payload map[string]interface{}, scopesClaim string, scope string) error {
-	scopes, exists := payload[scopesClaim]
-	if !exists {
-		return fmt.Errorf("scopes claim not found: %s", scopesClaim)
-	}
-
-	scopesArray, ok := scopes.([]interface{})
-	if !ok {
-		return fmt.Errorf("scopes claim improperly formatted: %s", scopesClaim)
-	}
-
-	_, ok = toMap(scopesArray)[scope]
-	if !ok {
-		return fmt.Errorf("scope not found: %s", scope)
-	}
-
-	return nil
-}
-
-func toMap(values []interface{}) map[string]int {
-	m := map[string]int{}
-	for i, v := range values {
-		m[v.(string)] = i
-	}
-
-	return m
 }
